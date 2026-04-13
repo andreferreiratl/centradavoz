f06a6bb8b3e196e1d70fdace0f67efe91db6b888
Andre Paula <criacao.tl67@gmail.com>
2026-04-13 17:12:49 +0000

    Visual edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -6,7 +6,7 @@ export default function Logo({ size = "default" }) {
     <Link to="/" className="flex items-center select-none">
       <img
         src="https://media.base44.com/images/public/69da50375cc9660ed0fab63a/c9d2e48da_file_000000009a48720eaaa61bf4a204e45c.png"
-        alt="Central da Voz" className="pr-4 pl-6 rounded-[14px] h-16 w-auto object-contain" />
+        alt="Central da Voz" className="pr-4 pl-6 rounded h-16 w-auto object-contain" />
 
       
     </Link>);
