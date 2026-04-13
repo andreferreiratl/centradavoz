b39ef1f130846734d97a912b865315bf3dc28c05
Andre Paula <criacao.tl67@gmail.com>
2026-04-13 17:15:22 +0000

    Visual edits

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,9 +4,9 @@ export default function Logo({ size = "default" }) {
   const isSmall = size === "sm";
   return (
     <Link to="/" className="flex items-center select-none">
-      <img
-        src="https://media.base44.com/images/public/69da50375cc9660ed0fab63a/c9d2e48da_file_000000009a48720eaaa61bf4a204e45c.png"
-        alt="Central da Voz" className="pr-4 pl-6 rounded h-16 w-auto object-contain" />
+      <img src="https://media.base44.com/images/public/69da50375cc9660ed0fab63a/bbb726247_file_000000009a48720eaaa61bf4a204e45c.png"
+
+      alt="Central da Voz" className="pr-4 pl-6 rounded h-16 w-auto object-contain" />
 
       
     </Link>);
