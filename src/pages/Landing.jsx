e13984dda96083a9851f6b0458c122856a77f8b5
Andre Paula <criacao.tl67@gmail.com>
2026-04-14 02:06:16 +0000

    Visual edits

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -45,7 +45,7 @@ export default function Landing() {
               de alta qualidade
             </h1>
             
-            <p className="text-muted-foreground text-lg md:text-xl mb-8 leading-relaxed">Crie narrações para vídeos, spots comerci redes sociais em segundos
+            <p className="text-muted-foreground text-lg md:text-xl mb-8 leading-relaxed">Crie narrações para vídeos, spots comerciais em segundos
 
             </p>
             
